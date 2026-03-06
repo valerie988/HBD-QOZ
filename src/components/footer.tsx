c7261195804a0913863bb29e1822936afe1aaf38
macbook <macbook@MacBook-Air-de-macbook.local>
2026-03-06 18:31:09 +0100

    social media

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,10 +15,10 @@ const BirthdayFooter = () => {
 
       {/* Social Media Links */}
       <div className="flex gap-6 mt-4">
-        <a href="https://www.instagram.com/the_glow_rious_effect?igsh=MW5iYWpoMHJiM2llZg==" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.tiktok.com/@theglow_riouseffect?_r=1&_d=e5jk4k8dj6l1ce&sec_uid=MS4wLjABAAAAJo1vFFYog78t2a5tj2mnGcp6w0CbzLORj2VHYBkNl8GAHPHY_y5cTXQnFDBvEEDN&share_author_id=6850127733453816837&sharer_language=fr&source=h5_m&u_code=dmlhjdjf9693m9&item_author_type=2&utm_source=copy&tt_from=copy&enable_checksum=1&utm_medium=ios&share_link_id=1B982274-0574-4E64-B60B-28BE9BD419B1&user_id=7055343005200663557&sec_user_id=MS4wLjABAAAAdkoNE1hVIisjuB8ixGAuVvo78nULnGXFwW2PfxxQcjP-tMc_hYM3vyko3dI_8NjN&social_share_type=5&ug_btm=b6880,b5836&utm_campaign=client_share&share_app_id=1233" target="_blank" rel="noopener noreferrer">
           <FaInstagram className="text-white text-2xl hover:text-red-600 transition" />
         </a>
-        <a href="https://www.tiktok.com/@theglow_riouseffect?_r=1&_t=ZS-94RRc0e7ac9" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.tiktok.com/@theglow_riouseffect?_r=1&_d=e5jk4k8dj6l1ce&sec_uid=MS4wLjABAAAAJo1vFFYog78t2a5tj2mnGcp6w0CbzLORj2VHYBkNl8GAHPHY_y5cTXQnFDBvEEDN&share_author_id=6850127733453816837&sharer_language=fr&source=h5_m&u_code=dmlhjdjf9693m9&item_author_type=2&utm_source=copy&tt_from=copy&enable_checksum=1&utm_medium=ios&share_link_id=1B982274-0574-4E64-B60B-28BE9BD419B1&user_id=7055343005200663557&sec_user_id=MS4wLjABAAAAdkoNE1hVIisjuB8ixGAuVvo78nULnGXFwW2PfxxQcjP-tMc_hYM3vyko3dI_8NjN&social_share_type=5&ug_btm=b6880,b5836&utm_campaign=client_share&share_app_id=1233" target="_blank" rel="noopener noreferrer">
           <FaTiktok className="text-white text-2xl hover:text-black transition" />
         </a>
         <a
